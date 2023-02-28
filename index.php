34f8b5038a3c575ebe3bdbab95d95702ca0d032b
ARTHUR Félix <arthur.felix160@gmail.com>
2023-02-28 22:00:51 +0100

    base

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    include "./head.php";
+    include "./header.php";
+?>
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex flex-column justify-content-center">
+    <div class="container" data-aos="zoom-in" data-aos-delay="100">
+        <h1>ARTHUR Félix-Vincent</h1>
+        <p>Je suis un <span class="typed" data-typed-items="Développeur"></span></p>
+        <div class="social-links">
+            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            <a href="#" class="youtube"><i class="bx bxl-youtube"></i></a>
+        </div>
+    </div>
+</section><!-- End Hero -->
+
+<?php
+    include "./footer.php";
+?>
