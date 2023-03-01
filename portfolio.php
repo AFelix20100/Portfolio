15a85b30adf3cb8542d6afca77726844700a949b
ARTHUR Félix <arthur.felix160@gmail.com>
2023-03-01 22:49:40 +0100

    Ajout d'images + détails

--- portfolio.php
+++ portfolio.php
@@ -25,29 +25,29 @@ include "./header.php";
 
             <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
 
-                <div class="col-lg-4 col-md-6 portfolio-item filter-formation">
+                <div class="col-lg-4 col-md-6 portfolio-item filter-formation filter-pasfini">
                     <div class="portfolio-wrap">
                         <img src="assets/img/portfolio/matchjob.webp" class="img-fluid" alt="">
                         <div class="portfolio-info">
                             <h4>MatchJob</h4>
                             <p>Formation</p>
                             <div class="portfolio-links">
-                                <a href="assets/img/portfolio/matchjob.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                                <a href="assets/img/portfolio/matchjob.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title=""><i class="bx bx-plus"></i></a>
+                                <a href="./projects/portfolio-details-matchjob.php" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-lg-4 col-md-6 portfolio-item filter-formation">
+                <div class="col-lg-4 col-md-6 portfolio-item filter-formation filter-pasfini">
                     <div class="portfolio-wrap">
                         <img src="assets/img/portfolio/fulbank.webp" class="img-fluid" alt="">
                         <div class="portfolio-info">
                             <h4>Fulbank</h4>
                             <p>Formation</p>
                             <div class="portfolio-links">
-                                <a href="assets/img/portfolio/fulbank.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                                <a href="assets/img/portfolio/fulbank.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title=""><i class="bx bx-plus"></i></a>
+                                <a href="./projects/portfolio-details-fulbank.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
                             </div>
                         </div>
                     </div>
@@ -57,25 +57,25 @@ include "./header.php";
                     <div class="portfolio-wrap">
                         <img src="assets/img/portfolio/ZendyShop.webp" class="img-fluid" alt="">
                         <div class="portfolio-info">
-                            <h4>ZendyShop</h4>
+                            <h4><a href="http://felix.gsvcom.fr">ZendyShop</a></h4>
                             <p>Formation</p>
                             <div class="portfolio-links">
-                                <a href="assets/img/portfolio/ZendyShop.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                                <a href="assets/img/portfolio/ZendyShop.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title=""><i class="bx bx-plus"></i></a>
+                                <a href="./projects/portfolio-details-zendyshop.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-lg-4 col-md-6 portfolio-item filter-perso">
+                <div class="col-lg-4 col-md-6 portfolio-item filter-perso filter-pasfini">
                     <div class="portfolio-wrap">
                         <img src="assets/img/portfolio/filibus.webp" class="img-fluid" alt="">
                         <div class="portfolio-info">
                             <h4>Filibus</h4>
                             <p>Personnels</p>
                             <div class="portfolio-links">
-                                <a href="assets/img/portfolio/filibus.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                                <a href="assets/img/portfolio/filibus.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title=""><i class="bx bx-plus"></i></a>
+                                <a href=./projects/portfolio-details-filibus.htmll" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
                             </div>
                         </div>
                     </div>
@@ -88,8 +88,8 @@ include "./header.php";
                             <h4>New Super Mario Bros</h4>
                             <p>Formation</p>
                             <div class="portfolio-links">
-                                <a href="assets/img/portfolio/mario.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                                <a href="assets/img/portfolio/mario.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title=""><i class="bx bx-plus"></i></a>
+                                <a href="./projects/portfolio-details-mario.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
                             </div>
                         </div>
                     </div>
@@ -97,13 +97,41 @@ include "./header.php";
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-formation">
                     <div class="portfolio-wrap">
-                        <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
+                        <img src="assets/img/portfolio/findyourmovie.webp" class="img-fluid" alt="">
                         <div class="portfolio-info">
-                            <h4>Web 3</h4>
-                            <p>Web</p>
+                            <h4>FindYourMovie</h4>
+                            <p>Formation</p>
                             <div class="portfolio-links">
-                                <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                                <a href="assets/img/portfolio/findyourmovie.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title=""><i class="bx bx-plus"></i></a>
+                                <a href="./projects/portfolio-details-findyoumovie.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-perso">
+                    <div class="portfolio-wrap">
+                        <img src="assets/img/portfolio/verbes.webp" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Verbes Irréguliers</h4>
+                            <p>Personnels</p>
+                            <div class="portfolio-links">
+                                <a href="assets/img/portfolio/verbes.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title=""><i class="bx bx-plus"></i></a>
+                                <a href="./projects/portfolio-details-verbes.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item filter-perso">
+                    <div class="portfolio-wrap">
+                        <img src="assets/img/portfolio/fichiers.webp" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                            <h4>Générateur de fichiers</h4>
+                            <p>Personnels</p>
+                            <div class="portfolio-links">
+                                <a href="assets/img/portfolio/fichiers.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title=""><i class="bx bx-plus"></i></a>
+                                <a href="./projects/portfolio-details-fichiers.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
                             </div>
                         </div>
                     </div>
@@ -111,55 +139,27 @@ include "./header.php";
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-formation">
                     <div class="portfolio-wrap">
-                        <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
+                        <img src="assets/img/portfolio/sophrologie.webp" class="img-fluid" alt="">
                         <div class="portfolio-info">
-                            <h4>Web 3</h4>
-                            <p>Web</p>
+                            <h4>Sophrologie</h4>
+                            <p>Formation</p>
                             <div class="portfolio-links">
-                                <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                                <a href="assets/img/portfolio/sophrologie.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title=""><i class="bx bx-plus"></i></a>
+                                <a href="./projects/portfolio-details-sophrologie.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-lg-4 col-md-6 portfolio-item filter-formation">
+                <div class="col-lg-4 col-md-6 portfolio-item filter-perso">
                     <div class="portfolio-wrap">
-                        <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
+                        <img src="assets/img/portfolio/sio.webp" class="img-fluid" alt="">
                         <div class="portfolio-info">
-                            <h4>Web 3</h4>
-                            <p>Web</p>
+                            <h4>SIO-SHARE</h4>
+                            <p>Personnels</p>
                             <div class="portfolio-links">
-                                <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6 portfolio-item filter-formation">
-                    <div class="portfolio-wrap">
-                        <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
-                        <div class="portfolio-info">
-                            <h4>Web 3</h4>
-                            <p>Web</p>
-                            <div class="portfolio-links">
-                                <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6 portfolio-item filter-formation">
-                    <div class="portfolio-wrap">
-                        <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
-                        <div class="portfolio-info">
-                            <h4>Web 3</h4>
-                            <p>Web</p>
-                            <div class="portfolio-links">
-                                <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                                <a href="assets/img/portfolio/sio.webp" data-gallery="portfolioGallery" class="portfolio-lightbox" title=""><i class="bx bx-plus"></i></a>
+                                <a href="./projects/portfolio-details-sio.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
                             </div>
                         </div>
                     </div>
